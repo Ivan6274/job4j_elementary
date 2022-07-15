5702d6af878d92479b1676d125bf9043c8da0cdd
is.ivanov <is.ivanov@action-media.ru>
2022-07-15 22:32:47 +0300

    5. Конвертер валюты [#41583 #321917]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -19,7 +19,6 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
         in = 120;
-        expected = 2;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
         System.out.println("120 rubles are 2 Test result : " + passed);
