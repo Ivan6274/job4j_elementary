e7d283f79f58119efafe6c643425531366e616eb
is.ivanov <is.ivanov@action-media.ru>
2022-07-26 08:14:17 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Sergey Sergeev";
+        names[3] = "Pavel Pavlov";
+
+        for (int i = 0; i <= names.length - 1 ; i++) {
+            System.out.println(names[i]);
+        }
 
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
