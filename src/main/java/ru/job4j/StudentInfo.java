316a0a727cb0d62bd8043b0344c1d5f21ae769b6
Ivan6274 <Isivanov2011@gmail.com>
2022-08-06 22:53:14 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,9 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-
         System.out.println("Ivan Ivanov");
         System.out.println("27.03.1986");
-
     }
 }
